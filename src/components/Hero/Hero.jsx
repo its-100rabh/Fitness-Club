f281ddbb16dc1f6fc0e3b6a5ea0d2c567153055c
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-07-08 10:26:18 +0530

    Updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,13 +3,34 @@ import "./Hero.css";
 import Header from "../Header/Header";
 
 const Hero = () => {
-  return <div className="hero">
-    <div className="left-h">
+  return (
+    <div className="hero">
+      <div className="left-h">
+        <Header />
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
 
-        <Header/>
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+          <div className="span">
+            <span>
+              One stop solution for building your ideal body and live up your
+              life to fullest.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="right-h">Right Side</div>
     </div>
-    <div className="right-h">Right Side</div>
-  </div>;
+  );
 };
 
 export default Hero;
