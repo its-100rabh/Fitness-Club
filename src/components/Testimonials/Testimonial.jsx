d8f8cb7d2b763408ec9d9f6f92e2b68811107597
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-07-15 21:51:20 +0530

    Testimonial Component Initialized

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Testimonial.css";
+
+const Testimonial = () => {
+  return (
+    <div className="testimonial">
+      <div className="left-t">
+        <span>Testimonials</span>
+        <span className="stroke-text">What people</span>
+        <span>say about us</span>
+      </div>
+      <div className="right-t"></div>
+    </div>
+  );
+};
+
+export default Testimonial;
