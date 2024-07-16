df07d16363982b7d73af33038b16d5481be4fea4
Saurabh Mahapatra <saurabhmahapatra03@gmail.com>
2024-07-16 12:48:57 +0530

    Content Added without permission

--- src/components/Testimonials/Testimonial.jsx
+++ src/components/Testimonials/Testimonial.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Testimonial.css";
+import { testimonialsData } from "../../data/testimonialsData";
+import leftArrow from "../../assets/leftArrow.png";
+import rightArrow from "../../assets/rightArrow.png";
 
 const Testimonial = () => {
+  const [selected, setSelected] = useState(0);
+  const tLength = testimonialsData.length;
+
   return (
     <div className="testimonial">
       <div className="left-t">
         <span>Testimonials</span>
         <span className="stroke-text">What people</span>
         <span>say about us?</span>
+        <span>{testimonialsData[selected].review}</span>
+        <span style={{ color: "var(--orange)" }}>
+          <span>{testimonialsData[selected].name}</span>
+        </span>{" "}
+        - {testimonialsData[selected].status}
       </div>
-      <div className="right-t"></div>
+      <div className="right-t">
+        <img src={testimonialsData[selected].image} alt="" />
+        <div className="arrows">
+          <img src={leftArrow} alt="" /> <img src={rightArrow} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
